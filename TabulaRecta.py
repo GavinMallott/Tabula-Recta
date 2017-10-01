a85ae6741c1db7a010eb08d530df9929c60b18c7
Gavin Mallott <gavinmallott@gmail.com>
2017-09-30 18:02:41 -0700

    Edit create_key.py and TabulaRecta.py
    Added a system for editing the key within the application.

--- TabulaRecta.py
+++ TabulaRecta.py
@@ -1,4 +1,5 @@
 from poly_alpha_cipher import PolyAlphabeticCipher as PAC
+from create_key import write_key_file
 
 
 def create_code():
@@ -16,14 +17,15 @@ def decipher_code():
 
 
 def main():
-    print("Greetings, and welcome to the improved encryption application.")
     print("What do you want to do?\n")
-    choice = int(input("1.) Create a secret code\n2.) Decipher an ecrypted message\n\n> "))
+    choice = int(input("1.) Create a secret code\n2.) Decipher an ecrypted message\n3.) Create a new key\n\n> "))
 
     if choice == 1:
         print(create_code().ciphertext)
     elif choice == 2:
         print(decipher_code().message)
+    elif choice == 3:
+        write_key_file(input("Input new key: "))
 
 
 if __name__ == "__main__":
